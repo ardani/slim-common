2965d1e86e1f5a0952a1a170b682b776c6d125d8
Aaron Collegeman <aaron@collegeman.net>
2014-03-24 19:47:34 -0400

    Added Embedly::extract

--- Embedly.php
+++ Embedly.php
@@ -18,4 +18,19 @@ class Embedly {
     }
   }
 
+  function extract($url, $opts = array()) {
+    $api = self::$root.'/extract?'.http_build_query(array_merge(array(
+      'key' => config('embedly.key'),
+      'url' => $url
+    ), $opts));
+
+    $req = Requests::get($api);
+    $res = json_decode($req->body);
+    if ($req->status_code !== 200) {
+      throw new Exception($res->error_message, $res->error_code);
+    } else {
+      return $res;
+    }
+  }
+
 }
