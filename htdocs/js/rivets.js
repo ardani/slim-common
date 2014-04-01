4f690aef165ceecb68a22ced935c83aa98671f98
Aaron Collegeman <aaron@collegeman.net>
2014-04-01 09:34:58 -0400

    Debugging rivets class- binder

--- htdocs/js/rivets.js
+++ htdocs/js/rivets.js
@@ -1123,6 +1123,7 @@
   };
 
   Rivets.binders['class-*'] = function(el, value) {
+    console.log(el, value);
     var elClass;
     elClass = " " + el.className + " ";
     if (!value === (elClass.indexOf(" " + this.args[0] + " ") !== -1)) {
